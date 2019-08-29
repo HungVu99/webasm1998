e1ae72b1661b58690a7bd6a5c937a3bd524d8a50
Hung Vu <hungvmgch17180@fpt.edu.vn>
2019-08-29 13:47:44 +0700

    a

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=ec2-174-129-27-158.compute-1.amazonaws.com port=5432 dbname=d692tlp6mgc3mc 
-user=qmiwpgmtylvvnq password=63251db4218016ad466a25a36fe6b2c76742cb871fa11da4a1051b7ebe4e3a71") 
+$conn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com port=5432 dbname=d4n9v5sup37lj7 
+user=hwcqtwzdvgituq password=61e60823007fb818a70633c69d2e7181549c60af271a4484793a5374ba270941") 
 or die("Connect Unsuccessful");
 ?>
