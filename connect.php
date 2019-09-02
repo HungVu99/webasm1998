90233873d95c26c74e80794365b8b1b8bd60d039
Hung Vu <hungvmgch17180@fpt.edu.vn>
2019-09-02 22:53:39 +0700

    ewqa

--- connect.php
+++ connect.php
@@ -3,3 +3,4 @@ $conn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com port=5432 db
 user=hwcqtwzdvgituq password=61e60823007fb818a70633c69d2e7181549c60af271a4484793a5374ba270941") 
 or die("Connect Unsuccessful");
 ?>
+a
