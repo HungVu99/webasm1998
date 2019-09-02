be70d85c0b4548a6ae237b76af6eb89da386eb98
Hung Vu <hungvmgch17180@fpt.edu.vn>
2019-09-02 10:36:22 +0700

    a

--- add.php
+++ add.php
@@ -21,7 +21,7 @@
                 $descrip = $_POST["descrip"];
                 $hinhanh=$_FILES['hinhanh']['name'];
                 $hinhanh_tmp=$_FILEs['hinhanh']['tmp_name'];
-                move_uploaded_file($hinhanh_tmp,'./images/'.$hinhanh);
+                move_uploaded_file($hinhanh_tmp,'./uploads/'.$hinhanh);
                 if ($name == ""||$price == ""|| $descrip == "") 
                     {
                         ?>
